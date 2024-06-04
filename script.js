dd8b178ee1ecb97549e0b806b2cf4c7327088ff9
Ahmed Hariri <150302563+Kodatchi-001@users.noreply.github.com>
2024-06-04 18:20:10 +0100

    create page-6 and responsiv

--- script.js
+++ script.js
@@ -13,3 +13,40 @@ function navbar_content_left() {
 function navbar_content_right() {
     navbarcontent.style.marginRight = '-150vw'
 }
+
+//card-information
+var image_card = document.querySelectorAll('.card-content-page-6 img');
+var paragraphe_card = document.querySelectorAll('.card-content-page-6-info p span');
+
+function card_information() {
+    const cards = [
+        //cards-1
+        {src : "Assets/card-1-page-6.png",paragraphe : "Absolutely thrilled with my experience. They were professional, creative, and incredibly talented. My home feels like a work of art now!"},
+        {src : "Assets/card-2-page-6.png",paragraphe : "They were fantastic to work with! Their design ideas were fresh and innovative, and they were always willing to accommodate my preferences."},
+        //cards-2
+        {src : "Assets/card-3-page-6.png",paragraphe : 'The experience was nothing short of exhilarating. The team displayed unparalleled professionalism, infused with creativity and talent.'},
+        {src : "Assets/card-4-page-6.png",paragraphe : 'Turning my home into such splendor is the result of a close collaboration with talented artists and designers.'},
+        //cards-3
+        {src : "Assets/card-5-page-6.png",paragraphe : 'I am deeply grateful for this transformation that has brought my wildest dreams to life. My home is now a sanctuary of beauty and inspiration.'},
+        {src : "Assets/card-6-page-6.png",paragraphe : 'Each room now breathes a unique aesthetic that not only mirrors the latest interior design trends but also my personal taste and personality.'},
+    ]
+    
+    image_card.forEach((img,index) => {
+       if(cards[index]){
+        img.src = cards[index].src;
+        paragraphe_card[index].textContent = cards[index].paragraphe
+       }
+    });
+    
+}
+card_information()
+//cards-slides
+let currentIndex = 0;
+const slides = document.querySelectorAll('.card-page-6');
+const totalSlides = slides.length;
+
+function moveSlide(direction) {
+    currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
+    const offset = -currentIndex * 100;
+    document.querySelector('.cards-page-6').style.transform = `translateX(${offset}%)`;
+}
