238df388ff523470613a51535f817d2f1996dc52
Ahmed Hariri <150302563+Kodatchi-001@users.noreply.github.com>
2024-06-05 14:38:56 +0100

    create page-7 and responsiv mobile

--- script.js
+++ script.js
@@ -21,23 +21,23 @@ var paragraphe_card = document.querySelectorAll('.card-content-page-6-info p spa
 function card_information() {
     const cards = [
         //cards-1
-        {src : "Assets/card-1-page-6.png",paragraphe : "Absolutely thrilled with my experience. They were professional, creative, and incredibly talented. My home feels like a work of art now!"},
-        {src : "Assets/card-2-page-6.png",paragraphe : "They were fantastic to work with! Their design ideas were fresh and innovative, and they were always willing to accommodate my preferences."},
+        { src: "Assets/card-1-page-6.png", paragraphe: "Absolutely thrilled with my experience. They were professional, creative, and incredibly talented. My home feels like a work of art now!" },
+        { src: "Assets/card-2-page-6.png", paragraphe: "They were fantastic to work with! Their design ideas were fresh and innovative, and they were always willing to accommodate my preferences." },
         //cards-2
-        {src : "Assets/card-3-page-6.png",paragraphe : 'The experience was nothing short of exhilarating. The team displayed unparalleled professionalism, infused with creativity and talent.'},
-        {src : "Assets/card-4-page-6.png",paragraphe : 'Turning my home into such splendor is the result of a close collaboration with talented artists and designers.'},
+        { src: "Assets/card-3-page-6.png", paragraphe: 'The experience was nothing short of exhilarating. The team displayed unparalleled professionalism, infused with creativity and talent.' },
+        { src: "Assets/card-4-page-6.png", paragraphe: 'Turning my home into such splendor is the result of a close collaboration with talented artists and designers.' },
         //cards-3
-        {src : "Assets/card-5-page-6.png",paragraphe : 'I am deeply grateful for this transformation that has brought my wildest dreams to life. My home is now a sanctuary of beauty and inspiration.'},
-        {src : "Assets/card-6-page-6.png",paragraphe : 'Each room now breathes a unique aesthetic that not only mirrors the latest interior design trends but also my personal taste and personality.'},
+        { src: "Assets/card-5-page-6.png", paragraphe: 'I am deeply grateful for this transformation that has brought my wildest dreams to life. My home is now a sanctuary of beauty and inspiration.' },
+        { src: "Assets/card-6-page-6.png", paragraphe: 'Each room now breathes a unique aesthetic that not only mirrors the latest interior design trends but also my personal taste and personality.' },
     ]
-    
-    image_card.forEach((img,index) => {
-       if(cards[index]){
-        img.src = cards[index].src;
-        paragraphe_card[index].textContent = cards[index].paragraphe
-       }
+
+    image_card.forEach((img, index) => {
+        if (cards[index]) {
+            img.src = cards[index].src;
+            paragraphe_card[index].textContent = cards[index].paragraphe
+        }
     });
-    
+
 }
 card_information()
 //cards-slides
@@ -50,3 +50,61 @@ function moveSlide(direction) {
     const offset = -currentIndex * 100;
     document.querySelector('.cards-page-6').style.transform = `translateX(${offset}%)`;
 }
+//cards-question
+function cards_question() {
+    var all_cards = document.querySelectorAll('.card-content-page-7');
+    var size = '25%'; 
+    var height ;
+    if (window.innerWidth <= 1400) {
+        height = '18%'
+    }
+    if (window.innerWidth <= 1024) {
+        height = '10vh'
+    }
+    var currentElement = null; 
+
+    all_cards.forEach(function(element) {
+        element.addEventListener('click', function() {
+            var icon = this.querySelector('.bx-chevron-down'); 
+            var tittles = this.querySelector(".card-content-quetion-page-7")
+            
+            if (currentElement && currentElement !== this) {
+                currentElement.style.height = '';
+                currentElement.style.alignItems = '';
+                var currentIcon = currentElement.querySelector('.bx-chevron-down');
+                var currentTitles = currentElement.querySelector(".card-content-quetion-page-7");
+                currentIcon.style.transform = '';
+                currentIcon.style.color = ''; 
+                currentTitles.style.height = '';
+            }
+            if (this.style.height == size) {
+                this.style.height = '';
+                this.style.alignItems = ''
+                icon.style.transform = ''; 
+                icon.style.height = ''; 
+                icon.style.color = ''; 
+                tittles.style.height = '';
+            } else {
+                if (currentElement) {
+                    currentElement.style.height = '';
+                    currentElement.alignItems = ''
+                    tittles.style.height = '';
+                    currentElement.querySelector('.bx-chevron-down').style.transform = '';
+                    icon.style.height = ''; 
+                    icon.style.color = ''; 
+                }
+                this.style.height = size;
+                this.style.alignItems = 'start'
+                this.style.transition = '2s'
+
+                icon.style.transform = 'rotate(180deg)';
+                icon.style.height = height;
+                icon.style.color = 'black';  
+                
+                tittles.style.height = height;
+                currentElement = this; 
+            }
+        });
+    });
+}
+cards_question();
